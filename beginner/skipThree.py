031bd465804af0453f27df5dc0f6bae8a4a8f62c
Lucy Murphy <74979154+lucyrosemurphy@users.noreply.github.com>
2021-05-12 08:58:28 -0400

    Create skipThree.py
    Create a program that prints numbers from 0 to 4, but skips 3. USE A WHILE LOOP

---
+++ beginner/skipThree.py
@@ -0,0 +1,16 @@
+CLASS WORK FEB-10-2020 LUCY MURPHY
+def skipitty(): #Print numbers from 0 to 4 but skip 3 (using a while loop)
+    i = 0
+    print (0) 
+    while i < 5:
+      i += 1
+      if i == 3:
+        continue #does not print
+      print(i)
+skipitty()
+
+#Example Output
+#>>>0
+#>>>1
+#>>>2
+#>>>4
